d2c7405561fb40a30557d9273e54ba5a775b6d36
pessy123 <31557847+pessy123@users.noreply.github.com>
2018-02-15 19:33:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/1',function(req,res){
+  res.send('article 1')  
+});
 app.get('/a1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'a1.html'));
 });
